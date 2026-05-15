406847239f846a09d9dc7b4ff87afdb374cd6750
eieiouka <eieiyukkuri@gmail.com>
2026-05-15 20:50:15 +0900

    k

---
+++ src/data/chapter2.js
@@ -0,0 +1,301 @@
+export const chapter2 = [
+  {
+    type: "trial",
+    speaker: "発言",
+    statementNo: 12,
+    side: "左",
+    target: "メモに書かれていた通り",
+
+    branch: {
+      index: "0/0",
+      text: "メモの内容と違う",
+    },
+
+    actions: [
+      {
+        type: "evidence",
+        index: "0/0",
+        text: "ハンナの録音データ",
+      },
+    ],
+  },
+
+  {
+    type: "trial",
+    speaker: "発言",
+    statementNo: 6,
+    side: "左",
+    target: "お腹を滅多刺しにして殺害した",
+
+    branch: {
+      index: "0/0",
+      text: "死因は腹部の刺し傷ではない",
+    },
+  },
+
+  {
+    type: "trial",
+    speaker: "発言",
+    statementNo: 12,
+    side: "左",
+    target: "鍵が閉まっていた",
+
+    branch: {
+      index: "0/0",
+      text: "元から開いてたのかも",
+    },
+
+    actions: [
+      {
+        type: "evidence",
+        index: "0/0",
+        text: "鐵副房の鍵",
+      },
+    ],
+  },
+
+  {
+    type: "trial",
+    speaker: "発言",
+    statementNo: 18,
+    side: "左",
+    target: "何かおかしい",
+
+    branch: {
+      index: "0/0",
+      text: "時系列が成立しない",
+    },
+
+    actions: [
+      {
+        type: "evidence",
+        index: "0/0",
+        text: "ハンナの録音データ",
+      },
+    ],
+  },
+
+  {
+    type: "trial",
+    speaker: "発言",
+    statementNo: 20,
+    side: "左",
+    target: "勘違い",
+
+    branch: {
+      index: "0/0",
+      text: "2人の勘違い",
+    },
+  },
+
+  {
+    type: "trial",
+    speaker: "発言",
+    statementNo: 11,
+    side: "左",
+    target: "声真似",
+
+    branch: {
+      index: "0/0",
+      text: "他に声を偽装する方法がある",
+    },
+
+    actions: [
+      {
+        type: "evidence",
+        index: "0/0",
+        text: "ハンナの録音データ",
+      },
+    ],
+  },
+
+  {
+    type: "trial",
+    speaker: "発言",
+    statementNo: 13,
+    side: "左",
+    target: "遠くから操作",
+
+    branch: {
+      index: "0/0",
+      text: "スマホが遠隔操作された",
+    },
+
+    actions: [
+      {
+        type: "choice",
+        index: "0/0",
+        text: "ナノカ以外にも消せる",
+      },
+
+      {
+        type: "person",
+        index: "0/0",
+        text: "憂目アンアン",
+      },
+    ],
+  },
+
+  {
+    type: "trial",
+    speaker: "発言",
+    statementNo: 15,
+    side: "左",
+    target: "意味",
+
+    branch: {
+      index: "0/0",
+      text: "偽装する意味がある",
+    },
+
+    actions: [
+      {
+        type: "choice",
+        index: "0/0",
+        text: "遠隔操作できない",
+      },
+    ],
+  },
+
+  {
+    type: "trial",
+    speaker: "発言",
+    statementNo: 12,
+    side: "左",
+    target: "不確実な事",
+
+    branch: {
+      index: "0/0",
+      text: "不確実ではない",
+    },
+
+    actions: [
+      {
+        type: "choice",
+        index: "0/0",
+        text: "魔法",
+      },
+    ],
+  },
+
+  {
+    type: "trial",
+    speaker: "発言",
+    statementNo: 9,
+    side: "左",
+    target: "誰でも部屋に入れた",
+
+    branch: {
+      index: "0/0",
+      text: "誰も部屋に入れない",
+    },
+
+    actions: [
+      {
+        type: "choice",
+        index: "0/0",
+        text: "ナノカにも扉を開ける事が不可能",
+      },
+
+      {
+        type: "choice",
+        index: "0/0",
+        text: "できる",
+      },
+
+      {
+        type: "person",
+        index: "0/0",
+        text: "ゴクチョー",
+      },
+    ],
+  },
+
+  {
+    type: "trial",
+    speaker: "発言",
+    statementNo: 15,
+    side: "左",
+    target: "共犯者",
+
+    branch: {
+      index: "0/0",
+      text: "看守が共犯はありえる",
+    },
+
+    actions: [
+      {
+        type: "choice",
+        index: "0/0",
+        text: "心当たりがある",
+      },
+
+      {
+        type: "person",
+        index: "0/0",
+        text: "憂目アンアン",
+      },
+    ],
+  },
+
+  {
+    type: "trial",
+    speaker: "発言",
+    statementNo: 9,
+    side: "左",
+    target: "誰も新たな情報",
+
+    branch: {
+      index: "0/0",
+      text: "新たな見解を示せる人がいる",
+    },
+
+    actions: [
+      {
+        type: "person",
+        index: "0/0",
+        text: "ゴクチョー",
+      },
+    ],
+  },
+
+  {
+    type: "trial",
+    speaker: "発言",
+    statementNo: 24,
+    side: "左",
+    target: "不具合",
+
+    branch: {
+      index: "0/0",
+      text: "スマホの説明に事件と矛盾がある",
+    },
+  },
+
+  {
+    type: "trial",
+    speaker: "発言",
+    statementNo: 12,
+    side: "左",
+    target: "スマホに仕掛け",
+
+    branch: {
+      index: "0/0",
+      text: "スマホはすり替えられていた",
+    },
+
+    actions: [
+      {
+        type: "choice",
+        index: "0/0",
+        text: "電話をかければ見つかる",
+      },
+
+      {
+        type: "person",
+        index: "0/0",
+        text: "佐伯ミリア",
+      },
+    ],
+  },
+]
--- src/data/chapters.js
+++ src/data/chapters.js
@@ -1,9 +1,15 @@
 import { chapter1 } from "./chapter1"
+import { chapter2 } from "./chapter2"
 
 export const chapters = [
   {
     id: "chapter1",
     title: "第1章",
     steps: chapter1,
+  },
+  {
+    id: "chapter2",
+    title: "第2章",
+    steps: chapter2,
   },
 ]
